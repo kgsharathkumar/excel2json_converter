827860ba4644e069de068e88747f264dd36cabb8
Sharathkumar K G <54583811+kgsharathkumar@users.noreply.github.com>
2020-06-30 18:47:30 +0530

    updated code
    updated code

--- Excel2JsonConverter.py
+++ Excel2JsonConverter.py
@@ -3,20 +3,20 @@ from collections import OrderedDict
 import simplejson as json
 
 # Open the workbook and select the first worksheet
-wb = xlrd.open_workbook('master_HMS_3P_Tools.xlsx')
+wb = xlrd.open_workbook('input_excel.xlsx')
 sh = wb.sheet_by_index(0)
 # List to hold dictionaries
-input_3P_checker_list = []
+input_checker_list = []
 # Iterate through each row in worksheet and fetch values into dict
 for rownum in range(1, sh.nrows):
     inputDataChecker = OrderedDict()
     row_values = sh.row_values(rownum)
-    inputDataChecker['3rd Party Library'] = row_values[0]
-    inputDataChecker['Supported in HMS'] = row_values[1]
-    inputDataChecker['Aditional Information'] = row_values[2]
+    inputDataChecker['1st Columns'] = row_values[0]
+    inputDataChecker['2nd Columns'] = row_values[1]
+    inputDataChecker['3rd Columns'] = row_values[2]
     input_3P_checker_list.append(inputDataChecker)
 # Serialize the list of dicts to JSON
-j = json.dumps(input_3P_checker_list)
+j = json.dumps(input_checker_list)
 # Write to file
 with open('data.json', 'w') as f:
     f.write(j)
